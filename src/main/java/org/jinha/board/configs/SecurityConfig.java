0f591bec106f46defd593b573493f0d7cc62f890
wlsgk12 <wlsgk9835@naver.com>
2023-05-15 11:52:19 +0900

    스프링 시큐리티 기본 설정, 정적 경로 설정, 임시 메인페이지 추가

---
+++ src/main/java/org/jinha/board/configs/SecurityConfig.java
@@ -0,0 +1,19 @@
+package org.jinha.board.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http.build();
+    }
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return w -> w.ignoring().requestMatchers("/css/**","/js/**","/images/**","/errors/**");
+    }
+}
