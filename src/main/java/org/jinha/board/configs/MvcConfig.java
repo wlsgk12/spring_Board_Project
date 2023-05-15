f074518c2c3dbd320c27557821ff63e07a691cd2
wlsgk12 <wlsgk9835@naver.com>
2023-05-15 12:53:44 +0900

    템플릿 설정 완료

--- src/main/java/org/jinha/board/configs/MvcConfig.java
+++ src/main/java/org/jinha/board/configs/MvcConfig.java
@@ -1,15 +1,21 @@
 package org.jinha.board.configs;
 
+import lombok.RequiredArgsConstructor;
+import org.jinha.board.configs.interceptors.SiteConfigInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class MvcConfig implements WebMvcConfigurer {
     @Value("${file.upload.path}")
     private String fileUploadPath;
+    //사이트 설정 유지 인터셉터
+    private final SiteConfigInterceptor siteConfigInterceptor;
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/")
@@ -20,6 +26,12 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:///"+fileUploadPath);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(siteConfigInterceptor)
+                .addPathPatterns("/**");
     }
 }
 
