7247a082a21b4931866d649eac60be9879f28267
wlsgk12 <wlsgk9835@naver.com>
2023-05-17 19:52:34 +0900

    공통 예외 처리중

---
+++ src/main/java/org/jinha/board/restcontrollers/CommonRestController.java
@@ -0,0 +1,7 @@
+package org.jinha.board.restcontrollers;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice("org.jinha.board.restcontrollers")
+public class CommonRestController {
+}
