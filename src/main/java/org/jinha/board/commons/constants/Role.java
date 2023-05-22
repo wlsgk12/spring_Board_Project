63999e63966c83202c89b15c957baff34e1ee39e
wlsgk12 <wlsgk9835@naver.com>
2023-05-22 19:03:32 +0900

    게시판 설정 구성중

--- src/main/java/org/jinha/board/commons/constants/Role.java
+++ src/main/java/org/jinha/board/commons/constants/Role.java
@@ -1,6 +1,7 @@
 package org.jinha.board.commons.constants;
 
 public enum Role {
+    ALL, //전체 사용자
     USER, //일반 사용자
     ADMIN //관리자
 }
--- src/main/java/org/jinha/board/controllers/admins/BoardController.java
+++ src/main/java/org/jinha/board/controllers/admins/BoardController.java
@@ -1,14 +1,15 @@
 package org.jinha.board.controllers.admins;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.jinha.board.commons.CommonException;
 import org.jinha.board.commons.MenuDetail;
 import org.jinha.board.commons.Menus;
+import org.modelmapper.internal.Errors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,7 +33,7 @@ public class BoardController {
      * @return
      */
     @GetMapping("/register")
-    public String register(Model model){
+    public String register(@ModelAttribute BoardForm boardForm, Model model){
         commonProcess(model, "게시판 등록");
         return "admin/board/config";
     }
@@ -40,6 +41,13 @@ public class BoardController {
     public String update(@PathVariable String bId, Model model){
         commonProcess(model, "게시판 수정");
         return "admin/board/config";
+    }
+    @PostMapping("/save")
+    public String save(@Valid BoardForm boardForm, Errors errors, Model model) {
+        String mode = boardForm.getMode();
+        commonProcess(model, mode != null && mode.equals("update") ? "게시판 수정" : "게시판 등록");
+
+        return "redirect:/admin/board"; // 게시판 목록
     }
 
     private void commonProcess(Model model, String title) {
